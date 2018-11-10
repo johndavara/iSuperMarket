518549f510865fbb8cdbdde08b65c60b9229715a
RobertoMJ <robertomj92@hotmail.com>
2018-11-10 12:02:01 -0600

    Vista de detalles de una novedad

---
+++ resources/views/newDetails.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <img class="card-img-top" src="{{$newDetails->image_url}}" alt="{{$newDetails->image_alt}}" title="{{$newDetails->image_title}}">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8 newDetails">
+            <div class="newTitle">
+                <h1>{{$newDetails->title}}</h1>
+            </div>
+            <div class="newDescription">
+                <pre>{{$newDetails->description}}</pre>
+            </div>
+            <div class="back">
+                <a href="/news" class="btn btn-primary">Volver a Novedades</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
